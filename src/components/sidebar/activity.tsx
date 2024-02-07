323820de6c136b57622fc20fe442297e49b09f98
Matthew Crist <hondo@Matthews-MacBook-Pro.local>
2024-02-07 12:35:20 -0500

    Comment out breaking chunk for now

--- src/components/sidebar/activity.tsx
+++ src/components/sidebar/activity.tsx
@@ -16,9 +16,9 @@ const Activity: React.FC<ActivityProps> = ({ activity }) => {
   return (
     <div className="mb-6">
       <h4 className="mb-2 font-bold text-sm uppercase">{activity.title}</h4>
-      {activity.section.map((section, index) => (
+      {/*activity.section.map((section, index) => (
         <div className="border-b border-gray-300 pb-2 mb-2" key={index}>{section.section_title}</div>
-      ))}
+      ))*/}
     </div>
   );
 };
