3a356319deded06e647ebf312d42387f2720d86e
Matthew Crist <hondo@Matthews-MacBook-Pro.local>
2024-03-22 21:57:21 -0400

    Adds nextui

---
+++ src/components/header.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button} from "@nextui-org/react";
+import Logo from "./logo";
+
+type HeaderProps = {
+  text: string;
+}
+
+const Header = (props: HeaderProps) => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+  ];
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <Link color="foreground" href="/units">
+            <Logo />
+          </Link>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="end">
+        <NavbarItem>
+          <Link color="foreground" href="/units">
+            Units
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} href="#" variant="flat">
+            Log in
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
+              }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+};
+
+export default Header;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const { nextui } = require("@nextui-org/react");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+	],
 	theme: {
 		extend: {},
 	},
 	plugins: [
 		require('@tailwindcss/aspect-ratio'),
+		nextui()
 	],
 }
