a40a1832087f83a65e557851abd9a1c10129e29a
Matthew Crist <hondo@Matthews-MacBook-Pro.local>
2024-06-09 21:09:23 -0400

    Slideshow basics

---
+++ src/components/slideshow/Slide.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { SlideTitle } from './slide-types/SlideTitle';
+import { SlideFlashCard } from './slide-types/SlideFlashCard';
+import { SlideExitTicket } from './slide-types/SlideExitTicket';
+import { SlideImage } from './slide-types/SlideImage';
+import { SlideText } from './slide-types/SlideText';
+
+type SlideProps = {
+  slide: {
+    sys: {
+        contentType: {
+            sys: {
+                id: string
+            }
+        }
+    }
+    fields: {
+        image?: {
+            fields: {
+                file: {
+                    url: string
+                }
+            }
+        };
+        title?: string;
+        description?: string;
+        questions?: string[];
+        content: {}
+    }
+  };
+}
+
+export const Slide: React.FC<SlideProps> = ({ slide }) => {
+    const slideType = slide.sys.contentType.sys.id;
+    let slideContent;
+
+    if (slideType === 'slideTitle') {
+        const title = slide.fields.title || '';
+        const imageUrl = slide.fields.image ? slide.fields.image.fields.file.url : '';
+        slideContent = <SlideTitle title={title} image={imageUrl} />;
+    }
+
+    if (slideType === 'slideFlashCard') {
+        slideContent = <SlideFlashCard cards={slide.fields.flashCards} />;
+    }
+
+    if (slideType === 'slideExitTicket') {
+        const title = slide.fields.title || '';
+        const questions = slide.fields.questions || [''];
+        slideContent = <SlideExitTicket title={title} questions={questions} />;
+    }
+
+    if (slideType === 'slideImage') {
+        const title = slide.fields.title || '';
+        const imageUrl = slide.fields.image ? slide.fields.image.fields.file.url : '';
+        slideContent = <SlideImage title={title} image={imageUrl} />;
+    }
+
+    if (slideType === 'slideText') {
+        const title = slide.fields.title || '';
+        const content = slide?.fields.content || '';
+        slideContent = <SlideText title={title} text={content} />;
+    }
+
+    return (
+        <div className="flex flex-col">
+            {slideContent}
+        </div>
+    );
+};
