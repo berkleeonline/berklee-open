19286f3463dd220ac461b61e2c09d9aa0a14297d
Steve Zimmerman <szimmerman@berklee.edu>
2024-07-14 11:45:37 -0400

    2-col numbering in right col to continue where left col OL left off in numbering sequence - this is a hack until we make it be a single column that computes, which may be  trickier than we think

--- src/components/slideshow/slide-types/SlideFlashCard.tsx
+++ src/components/slideshow/slide-types/SlideFlashCard.tsx
@@ -31,7 +31,7 @@ export const SlideFlashCard: React.FC<SlideFlashCardProps> = ({ cards }) => {
     const currentCard = cards[currentCardIndex];
     
     return (
-        <div className="flex flex-1 flex-col items-center justify-center w-screen py-10">
+        <div className="flex flex-1 flex-col items-center justify-center w-screen pt-10">
             <FlashCard card={currentCard} />
             <div className="hidden sm:flex gap-4 items-center rounded-xl bg-[rgba(0,0,0,0.5)] text-white py-6 px-10 mt-10">
                 <button onClick={goToNextCard} disabled={currentCardIndex === 0} className="rounded-full p-0 h-10 w-10 border border-white border-2 disabled:opacity-50">
