549a5eb4a01d8ac10f82955d46656d84f851413e
Steve Zimmerman <szimmerman@berklee.edu>
2024-09-19 14:06:51 -0400

    lesson work

--- src/components/tabs/lesson-tabs.tsx
+++ src/components/tabs/lesson-tabs.tsx
@@ -30,6 +30,7 @@ const materialIcons = {
   Keyboard: faPianoKeyboard,
   'Audio / Video': faPhotoFilmMusic,
   'Unpitched Instrument': faDrum,
+  'Audio Examples' : faMusic,
   Metronome: faTriangleInstrument,
   // Add more mappings as needed
 };
@@ -64,62 +65,102 @@ export const LessonTabs = ({
         <Tab key="overview" title="Overview" className="text-medium tracking-wider font-bold">
           <div className="pt-10 tracking-normal font-normal">
               
-              <div className="mb-16">
-                <IconHeader headerId="lessonStandards" icon={faStar} label="Standards" />
+              <div className="mb-8 pb-8 border-b">
+                <IconHeader headerId="lessonStandards" icon={faStar} label="Standards and Competencies" />
                 <ul className="list-disc list-outside ml-10 pl-4">
                   <li>Coming Soon</li>
                 </ul>
+                {lesson_sel && lesson_sel.length > 0 && (
+                  <div className={`mt-8 ${styles.richContentInnerStyles}`}>
+                    <h3 className="font-bold text-lg mb-4">Social-emotional Learning</h3>
+                    <div className="grid grid-cols-2 gap-2">
+                      {lesson_sel.map((sel, index) => (
+                        <div key={index} className="mb-1 flex flex-row items-center">
+                          <FontAwesomeIcon icon={materialIcons[sel] || faQuestionCircle} className="mr-3 w-4 h-4 text-center" />
+                          {sel}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                )}
               </div>
-            
-            {lesson_sel && lesson_sel.length > 0 && (
-              <div className="mb-16">
-                <IconHeader headerId="lessonSEL" icon={faStar} label="SEL" />
-                <ul className="list-disc list-outside ml-10 pl-4">
-                  {lesson_sel.map((sel, index) => (
-                    <li className="pb-3" key={index}>{sel}</li>
-                  ))}
-                </ul>
-              </div>
-            )}
-            
-            {lesson_prerequisites && lesson_prerequisites.content.length > 0 && (
-              <div className="mb-16">
-                <IconHeader headerId="lessonPrerequisites" icon={faSquareCheck} label="Prerequisites" />
-                <ul className="list-disc list-outside ml-10 pl-4">
-                  {lesson_prerequisites.content.map((prerequisite, index) => prerequisite.nodeType === 'unordered-list' &&
-                      prerequisite.content.map((item, idx) => <li className="pb-3" key={idx}>{item.content[0]?.content[0].value}</li>))}
-                </ul>
-              </div>
-            )}
+
+              {lesson_prerequisites && lesson_prerequisites.content.length > 0 && (
+                <div className="mb-8 pb-8 border-b">
+                    <IconHeader headerId="lessonPrerequisites" icon={faSquareCheck} label="Prerequisites" />
+                    {lesson_prerequisites.content.map((item, index) => {
+                      if (item.nodeType === 'unordered-list') {
+                        return (
+                          <ul className="list-disc list-outside ml-10 pl-4" key={index}>
+                            {item.content.map((listItem, idx) => {
+                              if (listItem.nodeType === 'list-item') {
+                                const paragraph = listItem.content[0];
+                                if (paragraph.nodeType === 'paragraph') {
+                                  return (
+                                    <li className="pb-3" key={idx}>
+                                      {paragraph.content.map((contentItem, contentIdx) => {
+                                        if (contentItem.nodeType === 'text') {
+                                          return <span key={contentIdx}>{contentItem.value}</span>;
+                                        } else if (contentItem.nodeType === 'embedded-entry-inline') {
+                                          const embeddedEntry = contentItem.data.target;
+                                          return (
+                                            <span key={contentIdx}>
+                                              {embeddedEntry?.sys?.id ? (
+                                                
+                                                <a href={`/lessons/${embeddedEntry.sys.id}`} target="_blank" className="underline hover:no-underline">
+                                                  {embeddedEntry?.fields?.lesson_title || 'Related Content'}
+                                                </a>
+                                              ) : (
+                                                'Related Content (No link available)'
+                                              )}
+                                            </span>
+                                          );
+                                        }
+                                        return null;
+                                      })}
+                                    </li>
+                                  );
+                                }
+                              }
+                              return null;
+                            })}
+                          </ul>
+                        );
+                      }
+                      return null;
+                    })}
+                  </div>
+                )}
+
 
 
             {lesson_outcome && lesson_outcome.content.length > 0 && (
-              <div className="mb-16">
+              <div className="mb-8 pb-8 border-b">
                 <IconHeader headerId="learningOutcomes" icon={faSeedling} label="Learning Outcomes" />
                 <h4 className="text-md font-bold pl-10 mb-2">Upon completion of this lesson, students will be able to:</h4>
                 <ul className="list-disc list-outside ml-10 pl-4">
                   {lesson_outcome.content?.map((outcome, index) => outcome.nodeType === 'unordered-list' &&
-                      outcome.content.map((item, idx) => <li className="pb-3" key={idx}>{item.content[0].content[0].value}</li>))}
+                      outcome.content.map((item, idx) => <li className="pt-3" key={idx}>{item.content[0].content[0].value}</li>))}
                 </ul>
               </div>
             )}
 
             {lesson_outline && lesson_outline.content.length > 0 && (
-              <div className="mb-16">
+              <div className="mb-8 pb-8 border-b">
                 <IconHeader headerId="lessonOutline" icon={faCompass} label="Lesson Outline" />
                 <ol className="list-decimal list-outside ml-10 pl-4">
                   {lesson_outline.content?.map((outline, index) => outline.nodeType === 'ordered-list' &&
-                      outline.content.map((item, idx) => <li className="pb-3" key={idx}>{item.content[0].content[0].value}</li>))}
+                      outline.content.map((item, idx) => <li className="pt-3" key={idx}>{item.content[0].content[0].value}</li>))}
                 </ol>
               </div>
             )}
 
             {lesson_essential_questions && lesson_essential_questions.length > 0 && (
-              <div className="mb-16">
+              <div className="mb-8 pb-8 border-b">
                 <IconHeader headerId="lessonEssentialQuestions" icon={faSealQuestion} label="Essential Questions" />
                 <ol className="list-decimal list-outside ml-10 pl-4">
                   {lesson_essential_questions?.map((question, index) => (
-                    <li className="pb-3" key={index}>
+                    <li className="pt-3" key={index}>
                       {question.fields?.question}
                     </li>
                   ))}
@@ -128,13 +169,13 @@ export const LessonTabs = ({
             )}
 
             {(lesson_prep || lesson_materials.length > 0) && (
-              <div className="mb-16">
+              <div className="mb-8 pb-8 border-b">
                 <IconHeader headerId="lessonPreparation" icon={faChalkboard} label="Preparation" />
 
                 {lesson_materials.length > 0 && (
-                  <div className={`mb-7 border-b pb-7 ${styles.richContentInnerStyles}`}>
+                  <div className={`${styles.richContentInnerStyles}`}>
                     <h3 className="font-bold text-lg mb-4">Referenced Materials</h3>
-                    <div className="grid grid-cols-2 gap-4">
+                    <div className="grid grid-cols-2 gap-2">
                       {lesson_materials?.map((material, index) => (
                         <div key={index} className="mb-1 flex flex-row items-center">
                           <FontAwesomeIcon icon={materialIcons[material] || faQuestionCircle} className="mr-3 w-4 h-4 text-center" />
@@ -152,22 +193,18 @@ export const LessonTabs = ({
             )}
 
             {lesson_repertoire && lesson_repertoire.content.length > 0 && (
-              <div className="mb-16">
+              <div className="mb-8 pb-8 border-b">
                 <IconHeader headerId="lessonRepertoire" icon={faMusic} label="Repertoire" />
-                <div className={`mb-7 ${styles.richContentInnerStyles}`} dangerouslySetInnerHTML={{ __html: documentToHtmlString(lesson_repertoire) }}></div>
+                <div className={` ${styles.richContentInnerStyles}`} dangerouslySetInnerHTML={{ __html: documentToHtmlString(lesson_repertoire) }}></div>
               </div>
             )}
 
             {lesson_evidence && lesson_evidence.content.length > 0 && (
-              <div className="mb-16">
+              <div className="mb-8 pb-8 border-b">
                 <IconHeader headerId="lessonEvidence" icon={faMusicMagnifyingGlass} label="Evidence of Learning" />
-                <div className={`mb-7 ${styles.richContentInnerStyles}`} dangerouslySetInnerHTML={{ __html: documentToHtmlString(lesson_evidence) }}></div>
+                <div className={`${styles.richContentInnerStyles}`} dangerouslySetInnerHTML={{ __html: documentToHtmlString(lesson_evidence) }}></div>
               </div>
             )}
-
-            
-
-            
           </div>
         </Tab>
         <Tab key="instructions" title="Instructions" className="tab-panel-instructions text-medium tracking-wider font-bold">
@@ -207,7 +244,7 @@ export const LessonTabs = ({
             )}
           </div>
         </Tab>
-        <Tab key="differentiated-strategies" title="Differentiated Strategies" className="text-medium tracking-wider font-bold">
+        <Tab key="differentiated-strategies" title="Strategies" className="text-medium tracking-wider font-bold">
           <div className="pt-10 tracking-normal font-normal">
             
             {lesson_accessibility && lesson_accessibility.content.length > 0 && (
