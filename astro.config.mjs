231f31fe410a07facea99e3943ee594862f36b51
Steve Zimmerman <szimmerman@berklee.edu>
2025-01-15 16:59:41 -0500

    favicons across the app, with a size for everone

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sass from 'sass';
+import favicons from "astro-favicons";
+
+import favicons from 'astro-favicons';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    react(),
-  ],
+  integrations: [tailwind(), react(), favicons()],
   css: {
     preprocessorOptions: {
       scss: {
