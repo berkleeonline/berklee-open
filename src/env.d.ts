03ae1d54d5cc0b05bdad32e1dc9f44b8fe9ad667
Jeff Trudeau <jeff.trudeau@gmail.com>
2025-01-31 11:26:40 -0500

    Inject PUBLIC_* versions of env vars into docker

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,4 +5,7 @@ interface ImportMetaEnv {
     readonly CONTENTFUL_SPACE_ID: string;
     readonly CONTENTFUL_DELIVERY_TOKEN: string;
     readonly CONTENTFUL_PREVIEW_TOKEN: string;
+    readonly PUBLIC_CONTENTFUL_SPACE_ID: string;
+    readonly PUBLIC_CONTENTFUL_DELIVERY_TOKEN: string;
+    readonly PUBLIC_CONTENTFUL_PREVIEW_TOKEN: string;
 }
